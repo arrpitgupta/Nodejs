caa34a5a9f070d6f1174581d28c393cee6076a92
arrpitgupta <arpit17200@gmail.com>
2025-09-04 09:24:33 +0530

    changes in add file

--- AddSubtract.js
+++ AddSubtract.js
@@ -1,3 +1,26 @@
-const {add, subtract} = require('./module')
-console.log(add(2 , 4));
-console.log(subtract(5 , 2));
+// const { add, subtract } = require("./module");
+// console.log(add(2, 4));
+// console.log(subtract(5, 2));
+function examplePromise(url) {
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+      resolve(`promise is complete @ ${url}`);
+    } else {
+      reject("error occored");
+    }
+  });
+}
+examplePromise('www.arpit.com')
+.then((res)=>{
+    console.log(res);
+    
+})
+.catch(()=>{
+    console.error("some error oucrred")
+}
+)
+.finally(()=>{
+    console.log("opp complete");
+    
+})
